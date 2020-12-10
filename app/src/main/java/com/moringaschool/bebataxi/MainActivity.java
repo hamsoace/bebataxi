09bb776d935fc83cd31c2b80c4a4aa76daeb9e5a
hamsoace <hamsoace@gmail.com>
2020-12-10 08:42:41 +0300

    driver login

--- app/src/main/java/com/moringaschool/bebataxi/MainActivity.java
+++ app/src/main/java/com/moringaschool/bebataxi/MainActivity.java
@@ -2,13 +2,30 @@ package com.moringaschool.bebataxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mDriver, mRider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDriver = (Button) findViewById(R.id.driver);
+        mRider = (Button) findViewById(R.id.rider);
+
+        mDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/moringaschool/bebataxi/RiderLoginActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.bebataxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RiderLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider_login);
+    }
+}
